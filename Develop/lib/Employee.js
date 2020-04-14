9991fe58838b88da4dc2855260f24c2d76b7fe94
Andrew Thomas <athomik79@gmail.com>
2020-04-14 13:19:42 -0400

    Employee parent class with properties and methods to Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(id, name, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.title = 'Employee';
+    }
+
+    // Methods
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return this.title;x
+    }
+
+}
+
+module.exports = Employee; 
